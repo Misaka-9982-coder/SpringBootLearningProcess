90f37cfc7284fe82a98ccc06b5497c4a6f629cb0
misaka <1784321894@qq.com>
2022-07-07 15:52:41 +0800

    SpringBoot-pre: Use JavaConfig to create a Object

---
+++ SpringBoot-pre/src/main/java/com/bjpowernode/config/SpringConfig.java
@@ -0,0 +1,41 @@
+package com.bjpowernode.config;
+
+import com.bjpowernode.vo.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// 表示当前类作为配置类使用的：配置容器
+@Configuration
+public class SpringConfig {
+
+    /*
+     * 创建方法，方法的返回值是对象，在方法的上面加入@Bean
+     * 方法的返回值对象就注入到容器中
+     *
+     * @Bean: 把对象注入到 Spring 容器中， 作用相当于 <bean></bean>
+     *
+     * 说明：@Bean，不指定对象的名称，默认的名称就是方法名
+     */
+    @Bean
+    public Student createStudent() {
+        Student student = new Student();
+        student.setName("张三");
+        student.setAge(20);
+        student.setSex("男");
+        return student;
+    }
+
+    /*
+     * 指定对象在容器中的名称（指定<bean>的 id 属性）
+     * @Bean 的 name 属性，可以指定对象的名称
+     */
+    @Bean(name = "LiSiStudent")
+    public Student makeStudent() {
+        Student student1 = new Student();
+        student1.setName("李四");
+        student1.setAge(21);
+        student1.setSex("男");
+        return student1;
+    }
+}
--- SpringBoot-pre/src/test/java/com/bjpowernode/TestStudent.java
+++ SpringBoot-pre/src/test/java/com/bjpowernode/TestStudent.java
@@ -1,16 +1,32 @@
 package com.bjpowernode;
 
+import com.bjpowernode.config.SpringConfig;
 import com.bjpowernode.vo.Student;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestStudent {
     @Test
-    public void test01() {
+    public void testBeansTagStudent() {
         String config = "beans.xml";
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
         Student student = (Student) ctx.getBean("myStudent");
         System.out.println("容器中的对象：" + student);
+    }
+
+    @Test
+    public void testBeansConfigStudent() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student student = (Student) ctx.getBean("createStudent");
+        System.out.println("使用JavaConfig创建的对象：" + student);
+    }
+
+    @Test
+    public void testBeansConfigStudent2() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student student = (Student) ctx.getBean("LiSiStudent");
+        System.out.println("使用JavaConfig创建的对象：" + student);
     }
 }
