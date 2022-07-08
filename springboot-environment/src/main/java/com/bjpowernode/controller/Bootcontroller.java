3c40d84ec809e2733ec8b7c03740d4d3fcebca92
misaka <1784321894@qq.com>
2022-07-08 09:39:21 +0800

    SpringBoot-MVC: use SpringApplication to return a success

---
+++ springboot-environment/src/main/java/com/bjpowernode/controller/Bootcontroller.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Bootcontroller {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String doSome() {
+        return "success";
+    }
+}
