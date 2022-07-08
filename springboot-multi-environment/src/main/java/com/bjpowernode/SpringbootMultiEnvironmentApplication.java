d007c1a93b5eea2351f114f8dfc08da5f5a56e13
misaka <1784321894@qq.com>
2022-07-08 10:02:26 +0800

    SpringBoot-MVC: use SpringApplication's multi .yml file to fix different ports and contexts

---
+++ springboot-multi-environment/src/main/java/com/bjpowernode/SpringbootMultiEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultiEnvironmentApplication.class, args);
+    }
+
+}
